9131ba6a1e1dd45efbbcfcdaf30ca9a3e10bb2ad
Daniela <dare0002@stud.kea.dk>
2024-10-04 13:04:05 +0200

    profanity done

---
+++ profanity_prep/profanity.js
@@ -0,0 +1,19 @@
+
+
+let theText = "Jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
+
+const words = theText.split("ord").join("ting");
+
+console.log(`${words}`);
+
+
+
+// Find ordet "ord" i strengen theText og erstat den med ting 
+
+// Prøv med split / join.
+
+// Prøv med replaceAll()
+
+// console.log dit resultat
+
+
